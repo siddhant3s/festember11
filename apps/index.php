5c6332a1511c463b48de2f66903dee36087ec348
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 04:46:33 +0530

    modified the stream publish message

--- apps/index.php
+++ apps/index.php
@@ -1,12 +1,49 @@
+<?php
+session_start();
+
+require_once "facebook/src/facebook.php";
+include_once "../facebook_details.php";//should contain app_id and app_secrete
+$fbuser=$facebook->getUser();
+$fbperm=array();
+$fbperm['scope'] = "email,publish_stream";
+
+$user = $facebook->api("/me");
+?>
 <!doctype html>
 <html>
   <head>
     <title>Festember Games</title>
-    <style>
-      body {width:760px;}
-    </style>
+    <script src="http://connect.facebook.net/en_US/all.js"></script>
+    <script>
+      var appId = <?php echo $facebook->getAppId(); ?>;
+    </script>
   </head>
   <body>
-    Festember11 Games coming up soon!
+    <h3>Festember11 Games coming up soon!</h3>
+    Hi <?php echo $user["name"]; ?>!<br>
+    <div id="fb-root"></div>
+
+    <script>
+      FB.init({
+        appId:appId
+        status:true,
+        cookie:true,
+        fbml:true,
+        oauth:true,
+      });
+      function pub() {
+        FB.ui({
+          method:"feed",
+          name:"<?php echo $user["name"]; ?> has won the game of roulette in Festember Casino!",
+          link:"http://www.festember.in/11/games/",
+          picture:"http://www.destination360.com/north-america/us/ne
+          vada/images/s/nevada-silver-legacy-resort-casino.jpg",
+          caption:"Casino games at Festember 11",
+          description:"Play the game now to get goodies and stuff",
+       });
+      }
+    </script>
+    You have won the game of roulette. <input type="button" value="Share it with your friends" onclick="pub();">
+
   </body>
 </html>
