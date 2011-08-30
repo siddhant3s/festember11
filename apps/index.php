a92e9b34275e8136aa9703f90bbdf2d37df59f02
Boopathi Rajaa <boopathi@live.com>
2011-08-31 04:37:43 +0530

    modified

--- apps/index.php
+++ apps/index.php
@@ -1,4 +1,12 @@
-<?php
-
-echo "Apps Coming Soon...";
-?>
+<!doctype html>
+<html>
+  <head>
+    <title>Festember Games</title>
+    <style>
+      body {width:760px;}
+    </style>
+  </head>
+  <body>
+    Festember11 Games coming up soon!
+  </body>
+</html>
