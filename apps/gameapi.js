d4f07838e9f42768fffc644aa5b5a83eb8e2426d
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 05:12:07 +0530

    small syntactic error in the last commit... Fixed that

--- apps/gameapi.js
+++ apps/gameapi.js
@@ -7,14 +7,4 @@ window.onload = function() {
         fbml:true,
         oauth:true,
       });
-      function pub() {
-        FB.ui({
-          method:"feed",
-          name:"<?php echo $user["name"]; ?> has won the game of roulette in Festember Casino!",
-          link:"http://www.festember.in/11/games/",
-          picture:"http://www.destination360.com/north-america/us/nevada/images/s/nevada-silver-legacy-resort-casino.jpg",
-          caption:"Casino games at Festember 11",
-          description:"Play the game now to get goodies and stuff",
-       });
-      }
 };
