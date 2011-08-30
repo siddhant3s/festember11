e3db2f056991a93d279b2e5400579cda899f7292
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 05:01:08 +0530

    created the new game library

---
+++ apps/gameapi.js
@@ -0,0 +1,17 @@
+      FB.init({
+        appId:appId,
+        status:true,
+        cookie:true,
+        fbml:true,
+        oauth:true,
+      });
+      function pub() {
+        FB.ui({
+          method:"feed",
+          name:"<?php echo $user["name"]; ?> has won the game of roulette in Festember Casino!",
+          link:"http://www.festember.in/11/games/",
+          picture:"http://www.destination360.com/north-america/us/nevada/images/s/nevada-silver-legacy-resort-casino.jpg",
+          caption:"Casino games at Festember 11",
+          description:"Play the game now to get goodies and stuff",
+       });
+      }
--- apps/index.php
+++ apps/index.php
@@ -1,13 +1,5 @@
 <?php
-session_start();
-
-require_once "facebook/src/facebook.php";
-include_once "../facebook_details.php";//should contain app_id and app_secrete
-$fbuser=$facebook->getUser();
-$fbperm=array();
-$fbperm['scope'] = "email,publish_stream";
-
-$user = $facebook->api("/me");
+include("fb.php");
 ?>
 <!doctype html>
 <html>
@@ -15,7 +7,7 @@ $user = $facebook->api("/me");
     <title>Festember Games</title>
     <script src="http://connect.facebook.net/en_US/all.js"></script>
     <script>
-      var appId = <?php echo $facebook->getAppId(); ?>;
+      var appId = <?php echo $facebook->getAppId(); ?>; //<?php echo $user["id"] ?>
     </script>
   </head>
   <body>
@@ -23,25 +15,7 @@ $user = $facebook->api("/me");
     Hi <?php echo $user["name"]; ?>!<br>
     <div id="fb-root"></div>
 
-    <script>
-      FB.init({
-        appId:appId,
-        status:true,
-        cookie:true,
-        fbml:true,
-        oauth:true,
-      });
-      function pub() {
-        FB.ui({
-          method:"feed",
-          name:"<?php echo $user["name"]; ?> has won the game of roulette in Festember Casino!",
-          link:"http://www.festember.in/11/games/",
-          picture:"http://www.destination360.com/north-america/us/nevada/images/s/nevada-silver-legacy-resort-casino.jpg",
-          caption:"Casino games at Festember 11",
-          description:"Play the game now to get goodies and stuff",
-       });
-      }
-    </script>
+    <script src="gameapi.js"></script>
     You have won the game of roulette. <input type="button" value="Share it with your friends" onclick="pub();">
 
   </body>
