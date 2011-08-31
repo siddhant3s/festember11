f3330aef39a308baddfd8f6fc8b82b1f1080bd39
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 07:16:09 +0530

    small change to the index.php file of games dir

--- games/index.php
+++ games/index.php
@@ -1,9 +1,12 @@
 <!doctype html>
 <html>
-<head>
-<title>Festember Games</title>
-</head>
-<body>
-Festember11 Games coming up soon!
-</body>
+  <head>
+    <title>Festember Games</title>
+    <style>
+      body {width:760px;}
+    </style>
+  </head>
+  <body>
+    Festember11 Games coming up soon!
+  </body>
 </html>
