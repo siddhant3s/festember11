39c0f2a6e205d7f20b4519c8765328496b52b79d
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 07:17:27 +0530

    added trace to num rows

--- apps/casino/index.php
+++ apps/casino/index.php
@@ -8,6 +8,7 @@
 	$_SESSION['user']=$user["name"];
 	error_log("###### " . "SELECT * FROM `user` WHERE `userid` = '" . $user["id"] . "'");
 	$res = mysql_query("SELECT * FROM `user` = '" . $user["id"] . "'");
+	error_log("###### num rows - " . mysql_num_rows($res));
 	if(mysql_num_rows($res) == 0) {
 	mysql_query("INSERT INTO `user` (`userid`,`money`) VALUES ('" . $user["id"] . "','1000')");
 	error_log("###### new user added to the table.");
