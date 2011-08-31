27500167b8271eaf8667659901fce8661f1f8158
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 07:32:40 +0530

    modified the mysql query

--- apps/casino/index.php
+++ apps/casino/index.php
@@ -7,9 +7,9 @@
 	
 	$_SESSION['user']=$user["name"];
 	error_log("###### " . "SELECT * FROM `user` WHERE `userid` = '" . $user["id"] . "'");
-	$res = mysql_fetch_assoc(mysql_query("SELECT COUNT(*) AS `count` FROM `user` = '" . $user["id"] . "'"));
+	$res = mysql_query("SELECT COUNT(*) AS `count` FROM `user` WHERE `userid` = '" . $user["id"] . "'");
 	error_log("###### num rows - " . $res['count']);
-	if(!$res) {
+	if(mysql_num_rows($res) == 0) {
 	mysql_query("INSERT INTO `user` (`userid`,`money`) VALUES ('" . $user["id"] . "','1000')");
 	error_log("###### new user added to the table.");
 	}
