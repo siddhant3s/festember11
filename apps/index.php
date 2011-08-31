e79c82e6e0c9cf23656cfc12c0693303d478aa6c
Shiva Nandan <seekshiva@gmail.com>
2011-08-31 06:03:47 +0530

    testing the use of json object for abstracting using the function

--- apps/index.php
+++ apps/index.php
@@ -9,26 +9,25 @@ include("fb.php");
     <script src="gameapi.js"></script>
     <script>
       var appId = <?php echo $facebook->getAppId(); ?>;
-      function pub() {
-        FB.ui({
-          method:"feed",
+      function pub(o) {
+        o.method = "feed";
+        FB.ui(o);
+      }
+      
+      obj = {
           name:"<?php echo $user["name"]; ?> has won the game of roulette in Festember Casino!",
           link:"http://www.festember.in/11/games/",
           picture:"http://www.destination360.com/north-america/us/nevada/images/s/nevada-silver-legacy-resort-casino.jpg",
           caption:"Casino games at Festember 11",
           description:"Play the game now to get goodies and stuff",
-       });
-      }
-
+       }
     </script>
   </head>
   <body>
     <h3>Festember11 Games coming up soon!</h3>
     Hi <?php echo $user["name"]; ?>!<br>
     <div id="fb-root"></div>
-
-
-    You have won the game of roulette. <input type="button" value="Share it with your friends" onclick="pub();">
+    You have won the game of roulette. <input type="button" value="Share it with your friends" onclick="pub(obj);">
 
   </body>
 </html>
