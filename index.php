0ee54f3da2824cfc31b192ed1efe085c1cd54ba5
Siddhant Sanyam <siddhant3s@gmail.com>
2011-08-31 05:34:43 +0530

    Added app_id and app_secrete detail in separate file which is not exposed in repo for security reason

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ $SUB_DIR = (dirname($_SERVER['SCRIPT_NAME'])=='/')?'/':dirname($_SERVER['SCRIPT_
 $FULLPATH = 'http://' . $DOMAIN_NAME . $SUB_DIR;
 header('X-XRDS-Location:' . $FULLPATH . 'yadis.xrdf');
 session_start();
-require_once("apps/facebook/src/facebook.php");
+
+require_once "apps/facebook/src/facebook.php";
+include_once "facebook_details.php";//should contain app_id and app_secrete
+
 /** <Login Related Shit **/
   try {
       function openid_auth($openid_url)
