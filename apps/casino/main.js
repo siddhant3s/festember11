838127c9276fc902a850b7e7b3d1bb9ebd0c904b
SHADY <shady@SHADY.(none)>
2011-09-07 03:56:45 +0530

    bugfixes in the display and hiding of inputs

--- apps/casino/main.js
+++ apps/casino/main.js
@@ -64,9 +64,11 @@ var status=0,curMoney=0,money,userid,card1,card2,card3,card4,card5,card6,card7,c
 			var u1,u2,d1,d2,c1,c2,c3,c4,c5,betMoney=0,temp,temp1,temp2,temp3,temp4;
 			if(curMoney!=0||status==2){
 				$("#bet").hide();
+				$("#bet").attr("disabled":"disabled");
 				status++;
 			if(status==2){
 			$("#bet").hide();
+			$("#bet").attr("disabled":"disabled");
 			$("#loading").show();
 			betMoney=curMoney;
 			curMoney=0;
@@ -114,7 +116,7 @@ var status=0,curMoney=0,money,userid,card1,card2,card3,card4,card5,card6,card7,c
 					setTimeout(function(){animate5(temp3);},200);
 					setTimeout(function(){animate6(temp4);},200);
 					setTimeout(function(){animate7(temp2);},400);
-					setTimeout(function(){$("#bet").show();},5000);
+					setTimeout(function(){$("#bet").show();$("#bet").removeAttr("disabled");},5000);
 			}});
 			setTimeout(function(){dat="money="+betMoney;
 						$.ajax({type: "POST",data:dat,url: "putmoney.php"});},600);
@@ -263,8 +265,8 @@ var status=0,curMoney=0,money,userid,card1,card2,card3,card4,card5,card6,card7,c
 						setTimeout(hey,700);	
 					}
 			$("#bet").show();
+			$("#bet").attr("disabled");
 			$("#sub").hide();
-			$("#money").hide();
 			$("#five").click(function(){});
 			$("#ten").click(function(){});
 			$("#twenty").click(function(){});
@@ -276,6 +278,7 @@ var status=0,curMoney=0,money,userid,card1,card2,card3,card4,card5,card6,card7,c
 						$("#loading").show();
 						$("#fold").hide();
 						$("#bet").hide();
+						$("#bet").attr({disabled:'disabled'});
 						betMoney=document.getElementById("bmoney").value;
 						betMoney*=3;
 						document.getElementById("bmoney").value=betMoney;
