2cad71dfcf76cc18494b177b241c1814056a2f6b
Siddharth Chan <chansiddharth@gmail.com>
2011-09-07 03:12:03 +0530

    added feature for inviting friends to play the game

--- apps/gameapi.js
+++ apps/gameapi.js
@@ -1,4 +1,3 @@
-
 FB.init({
     appId:appId,
     status:true,
@@ -7,3 +6,13 @@ FB.init({
     oauth:true,
 });
 
+
+function invite()
+{var request_ids=FB.ui({method: 'apprequests', message: 'I play festember games.I think that you should play it too', filters: 'app_non_users' , data: 'tracking information for the user'});
+$.ajax({
+   type: "POST",
+   url: "reqconnect.php",
+   data: "rid=requests_ids",
+
+ });
+}
