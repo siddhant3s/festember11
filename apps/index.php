c222230e299aa0d11d88cd5810b05f85b4e59bc1
shriram <shriram@shriram.(none)>
2011-09-07 03:48:49 +0530

    created the new home page for festember casino games

--- apps/index.php
+++ apps/index.php
@@ -1,37 +1,119 @@
-<?php
-$rpath = "";
-include("fb.php");
-?>
-<!doctype html>
 <html>
-  <head>
-    <title>Festember Games</title>
-    <style>
-      body {width:760px;margin: auto; }
-    </style>
-    <script src="http://connect.facebook.net/en_US/all.js"></script>
-    <script>
-      var appId = <?php echo $facebook->getAppId(); ?>;
-      function pub(o) {
-        o.method = "feed";
-        FB.ui(o);
-      }
-      
-      obj = {
-          name:"<?php echo $user["name"]; ?> has won the game of Roulette in Festember Casino!",
-          link:"http://www.festember.in/11/games/",
-          picture:"http://www.destination360.com/north-america/us/nevada/images/s/nevada-silver-legacy-resort-casino.jpg",
-          caption:"Casino games at Festember 11",
-          description:"Play the game now to get goodies and stuff",
-       }
-    </script>
-  </head>
-  <body>
-    <h3>Festember11 Games coming up soon!</h3>
-    Hi <?php echo $user["name"]; ?>!<br>
-    <div id="fb-root"></div>
-    <script src="gameapi.js"></script>
-    You have won the game of roulette. <input type="button" value="Share it with your friends" onclick="pub(obj);">
+<head>
+<title>FESTEMBER 11</title>
+<link href="main.css" type="text/css" rel="stylesheet"/>
+<script type="text/javascript" src="jquery.js" > 
+		</script>
+		
+<script type="text/javascript">
+function pokere()
+{
+$("#poker_img").css({
+"opacity":"1"
+});
 
-  </body>
-</html>
+}
+function pokerl()
+{
+$("#poker_img").css({
+"opacity":"0"
+});
+
+}
+
+
+
+
+
+
+function mankathae()
+{
+$("#mang_img").css({
+"opacity":"1"
+
+});
+
+}
+function mankathal()
+{
+$("#mang_img").css({
+"opacity":"0"
+});
+}
+
+$(document).ready(function() {
+
+    $("#roulette").hover(function() {
+        $("#roul_img").css({
+            "opacity":"1"
+        });
+    },function() {
+        $("#roul_img").css({
+            "opacity":"0"
+        });
+    });
+
+    $("#claw").hover(function() {
+        $("#claw_img").css({
+            "opacity":"1"
+        });
+    },function() {
+        $("#claw_img").css({
+            "opacity":"0"
+        });
+    });
+
+ $("#blackjack").hover(function() {
+        $("#bljack_img").css({
+            "opacity":"1"
+        });
+    },function() {
+        $("#bljack_img").css({
+            "opacity":"0"
+        });
+    });
+ 
+ $("#slotm").hover(function() {
+        $("#slot_img").css({
+            "opacity":"1"
+        });
+    },function() {
+        $("#slot_img").css({
+            "opacity":"0"
+        });
+    });
+
+ $("#slotm1").hover(function() {
+        $("#slot1_img").css({
+            "opacity":"1"
+        });
+    },function() {
+        $("#slot1_img").css({
+            "opacity":"0"
+        });
+    });
+ $("#ants").hover(function() {
+        $("#ants_img").css({
+            "opacity":"1"
+        });
+    },function() {
+        $("#ants_img").css({
+            "opacity":"0"
+        });
+    });
+
+});
+</script>
+</head>
+<body>
+<div id="div_game"><img src="images/main casino pg.jpg" alt="casino" />
+<div id="poker" onmouseover="pokere()" onmouseout="pokerl()"><a href="#"><img src="images/POKER1.png" alt="poker" width="380" id="poker_img"></a></div>
+<div id="roulette"><a href="#"><img src="images/roulette1.png" alt="roulette" width="450" id="roul_img"></a></div>
+<div id="mankatha" onmouseover="mankathae()" onmouseout="mankathal()"<a href="#">><img src="images/MANKATHA1.png" alt="mankatha" width="310" id="mang_img"></a></div>
+<div id="claw" ><a href="#"><img src="images/CLAWS1.png" alt="claw" width="500" id="claw_img"></a></div>
+<div id="blackjack"><a href="#"><img src="images/BLACK JACK1.png" alt="blackjack" width="530" id="bljack_img"></a></div>
+<div id="slotm"><a href="#"><img src="images/slot1.png" alt="slot" width="360" height="280" id="slot_img"></a></div>
+<div id="slotm1"><a href="#"><img src="images/slot1.png" alt="slot1" width="265" id="slot1_img"></a></div>
+<div id="ants"><a href="#"><img src="images/ants1.png" alt="slot" width="300"id="ants_img"></a></div>
+</div>
+</body></html>
