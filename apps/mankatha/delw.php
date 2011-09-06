db30936db63ccc98d4db355b95ca1e601fde35ad
Harish <harishsmart93@yahoo.co.in>
2011-09-07 04:56:11 +0530

    fb api checks

--- apps/mankatha/delw.php
+++ apps/mankatha/delw.php
@@ -33,7 +33,6 @@ mysql_query($query);
     </script>
 <script src="../../gameapi.js"></script>
 <script>
-alert("hi");
 function publish() {
 FB.ui({
   "name":"<?php echo $user["name"]; ?> is running short of free spins in the Festember Casino.",
@@ -45,11 +44,12 @@ FB.ui({
 //  to:"100000566828426",
 });
 </script>
-<body><img src="ropa.png" alt="" style="position:absolute;left:250px;height:655px;" />
+/*<body><img src="ropa.png" alt="" style="position:absolute;left:250px;height:655px;" />
 <div style="position:absolute; top:50%; left:45%; color:white;"?>
 You won this round.
 <a href="start.php" style="position:absolute;left:5%;top:70%;"><img src="bpa.png" border="0" alt=""/></a>
 <a href="index.php" style="position:absolute;left:5%;top:650%;"><img src="bq.png" border="0" alt=""/></a>
 </div>
-</body>
+publish();
+</body>*/
 </html>
