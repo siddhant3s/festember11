0df930210ff4160082b8ce49ef6b83602865ee88
Shiva Nandan <seekshiva@gmail.com>
2011-09-06 21:58:56 +0530

    possibly fixed the bug in sharing on friends wall

--- apps/Festigame.php
+++ apps/Festigame.php
@@ -19,8 +19,8 @@ include("fb.php");
 
 //print_r($facebook->api("/me/friends"));
 
-$arr["from"] = $user["id"];
-$arr["to"] = "100000566828426";
+//$arr["from"] = $user["id"];
+//$arr["to"] = "100000566828426";
 $arr["message"] = "hello!";
 $facebook->api("/me/feed",$arr);
 ?>
