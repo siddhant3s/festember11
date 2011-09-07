dfcb29ec1a3403e9456097770519519b8b55ead3
Shiva Nandan <seekshiva@gmail.com>
2011-09-07 13:38:13 +0530

    added the invite friends button

--- apps/index.php
+++ apps/index.php
@@ -1,9 +1,14 @@
+<?php
+$rpath = "";
+include("fb.php");
+?>
 <html>
 <head>
 <title>FESTEMBER 11</title>
 <link href="main.css" type="text/css" rel="stylesheet"/>
 <script type="text/javascript" src="jquery.js" ></script>
 <script type="text/javascript">
+  var appId = <?php echo $facebook->getAppId(); ?>;
   function pokere()
   {
     $("#poker_img").css({
@@ -96,8 +101,11 @@
 </script>
 </head>
 <body>
+<div id="fb-root"></div>
+<script src="gameapi.js"></script>
 
 <div style="position:absolute; background-color:rgba(225,225,225,.9); height:70px; width:100%;">
+<div style="float:right; "><input type="button" value="Invite" onclick="invite();"> your friends to play the game and gain more xp points</div>
 <iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fapps%2Fapplication.php%3Fid%3D263593520331172%26sk%3Dwall&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;border_color=%23444&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:62px;" allowTransparency="true" style="position:absolute; "></iframe>
 </div>
 
