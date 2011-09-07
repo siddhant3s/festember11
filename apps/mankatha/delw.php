d9687e7383a57cd2d3da403cff190c7cc332da51
Harish <harishsmart93@yahoo.co.in>
2011-09-07 19:03:06 +0530

    changed winning php for fb

--- apps/mankatha/delw.php
+++ apps/mankatha/delw.php
@@ -1,6 +1,8 @@
 <?php
 $rpath = "../";
 include("../fb.php");
+?>
+<?
 		session_start();
 $connection=mysql_connect("localhost","festember","vegas11");
 
@@ -24,7 +26,7 @@ if(!$db_select){
 mysql_query($query);
 ?>
 <html>
-<div id="fb-root"></div>
+<div id="fb-root"><script>publish()</script> </div>
 <script src="http://connect.facebook.net/en_US/all.js"></script>
     <script>
     var appId = <?php echo $facebook->getAppId(); ?>;
@@ -42,12 +44,12 @@ FB.ui({
 //  to:"100000566828426",
 });
 </script>
-/*<body><img src="ropa.png" alt="" style="position:absolute;left:250px;height:655px;" />
+<body><img src="ropa.png" alt="" style="position:absolute;left:250px;height:655px;" />
 <div style="position:absolute; top:50%; left:45%; color:white;"?>
 You won this round.
 <a href="start.php" style="position:absolute;left:5%;top:70%;"><img src="bpa.png" border="0" alt=""/></a>
 <a href="index.php" style="position:absolute;left:5%;top:650%;"><img src="bq.png" border="0" alt=""/></a>
 </div>
 publish();
-</body>*/
+</body>
 </html>
