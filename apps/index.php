4bce91335f91e9d7e0ae7e863f239eff32c7cb01
Shiva Nandan <seekshiva@gmail.com>
2011-09-07 21:14:10 +0530

    channged some styles

--- apps/index.php
+++ apps/index.php
@@ -101,12 +101,12 @@ include("fb.php");
 });
 </script>
 </head>
-<body>
+<body style="margin:0; ">
 <div id="fb-root"></div>
 <script src="gameapi.js"></script>
 
-<div style="position:absolute; background-color:rgba(225,225,225,.9); height:70px; width:100%;">
-<div style="float:right; "><input type="button" value="Invite" onclick="invite();"> your friends to play the game and gain more xp points</div>
+<div style="position:absolute; background-color:rgba(225,225,225,.9); height:70px; width:1366px; ">
+<div style="float:right; padding:5px; margin:5px; "><input type="button" value="Invite" onclick="invite();"> your friends to play the game and gain more xp points</div>
 <iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fapps%2Fapplication.php%3Fid%3D263593520331172%26sk%3Dwall&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;border_color=%23444&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:62px;" allowTransparency="true" style="position:absolute; "></iframe>
 </div>
 
