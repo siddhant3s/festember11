4bfde5932e34dff4c3656b3dbecc5200a91ea3f7
Shiva Nandan <seekshiva@gmail.com>
2011-09-07 13:11:36 +0530

    added the 'Like this App' link on top of the game page

--- apps/index.php
+++ apps/index.php
@@ -2,47 +2,37 @@
 <head>
 <title>FESTEMBER 11</title>
 <link href="main.css" type="text/css" rel="stylesheet"/>
-<script type="text/javascript" src="jquery.js" > 
-		</script>
-		
+<script type="text/javascript" src="jquery.js" ></script>
 <script type="text/javascript">
-function pokere()
-{
-$("#poker_img").css({
-"opacity":"1"
-});
+  function pokere()
+  {
+    $("#poker_img").css({
+      "opacity":"1"
+    });
+  }
+  function pokerl()
+    {
+    $("#poker_img").css({
+      "opacity":"0"
+    });
+  }
 
-}
-function pokerl()
-{
-$("#poker_img").css({
-"opacity":"0"
-});
+  function mankathae()
+  {
+    $("#mang_img").css({
+      "opacity":"1"
+    });
+  }
 
-}
-
-
-
-
-
-
-function mankathae()
-{
-$("#mang_img").css({
-"opacity":"1"
-
-});
-
-}
-function mankathal()
-{
-$("#mang_img").css({
-"opacity":"0"
-});
-}
-
-$(document).ready(function() {
-
+  function mankathal()
+  {
+    $("#mang_img").css({
+      "opacity":"0"
+    });
+  }
+  
+  $(document).ready(function() {
+  
     $("#roulette").hover(function() {
         $("#roul_img").css({
             "opacity":"1"
@@ -106,6 +96,8 @@ $(document).ready(function() {
 </script>
 </head>
 <body>
+<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fplatform&amp;width=292&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=true&amp;header=true&amp;height=590" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:590px;" allowTransparency="true"></iframe>
+
 <div id="div_game"><img src="images/main casino pg.jpg" alt="casino" />
 <div id="poker" onmouseover="pokere()" onmouseout="pokerl()"><a href="#"><img src="images/POKER1.png" alt="poker" width="380" id="poker_img"></a></div>
 <div id="roulette"><a href="#"><img src="images/roulette1.png" alt="roulette" width="450" id="roul_img"></a></div>
