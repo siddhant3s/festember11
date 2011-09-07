45418f53deda3e3aa631bb3c816bfb38df8033a1
Shiva Nandan <seekshiva@gmail.com>
2011-09-07 08:20:51 +0530

    modified index of app directory for some local testing

--- apps/index.php
+++ apps/index.php
@@ -25,12 +25,12 @@ include("fb.php");
           description:"Play the game now to get goodies and stuff",
        }
     </script>
-    <script src="gameapi.js"></script>
   </head>
   <body>
     <h3>Festember11 Games coming up soon!</h3>
     Hi <?php echo $user["name"]; ?>!<br>
     <div id="fb-root"></div>
+    <script src="gameapi.js"></script>
     You have won the game of roulette. <input type="button" value="Share it with your friends" onclick="pub(obj);">
 
   </body>
