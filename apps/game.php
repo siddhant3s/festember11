911da9cb0484eff3c3d47c7ebefdb27d803897c5
Shiva Nandan <seekshiva@gmail.com>
2011-09-08 16:38:25 +0530

    added the game api file

---
+++ apps/game.php
@@ -0,0 +1,17 @@
+<?php
+include("../connect.php");
+
+$query = "SELECT `bidamount`,`winvar` FROM `game_info` WHERE `playerid`='" . $user["id"]  . "'";
+$res = mysql_query($query);
+
+$cash = 1000;
+
+$sarr["0"] = -1;
+$sarr["1"] = 2;
+
+while ($row = mysql_fetch_array($res)) {
+    $cash += $row['bidamount'] * $row[$sarr['winvar']];
+}
+
+echo "You have " . $cash . " in your account!";
+?>
