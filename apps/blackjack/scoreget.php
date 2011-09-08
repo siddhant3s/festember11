29a27724003aa19016a972a53ec9bde656626663
Amrit <amritsahoo@gmail.com>
2011-09-08 19:08:56 +0530

    Included scoring system

---
+++ apps/blackjack/scoreget.php
@@ -0,0 +1,29 @@
+<?php
+
+
+include("../fb.php");
+include("../reqconnect.php");
+
+$sql="SELECT * FROM game_info WHERE playerid = '".$user[id]."'";
+
+$result = mysql_query($sql);
+
+echo "<table border='1'>
+<tr>
+<th>playerid</th>
+<th>gameid</th>
+<th>money</th>
+</tr>";
+
+while($row = mysql_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['playerid'] . "</td>";
+  echo "<td>" . $row['gameid'] . "</td>";
+  echo "<td>" . $row['money'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+?>
---
+++ apps/blackjack/scoreput.php
@@ -0,0 +1,24 @@
+<?php
+
+include("../fb.php");
+include("../reqconnect.php");
+$bid=0;
+$ret=0;
+$con = mysql_connect('localhost', 'festember11', 'abc123');
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("ajax_demo", $con);
+
+//$sql="SELECT * FROM game_info WHERE playerid = '".$user[id]."'";
+$sql="INSERT INTO game_info 
+(playerid,gameid,starttime,bidamount,returnpercent) VALUES('".$user[id]."',2,CURRENT_TIME_STAMP,'".$bid."','".$ret."' ) ";
+
+$result = mysql_query($sql);
+
+
+
+mysql_close($con);
+?>
