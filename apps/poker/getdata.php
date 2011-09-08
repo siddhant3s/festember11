65f4ee900f5ba1a9b3b6aafd4b73960d15b60a66
SHADY <shady@SHADY.(none)>
2011-09-09 00:56:50 +0530

    test

--- apps/poker/getdata.php
+++ apps/poker/getdata.php
@@ -1,22 +1,22 @@
 <?php include("../../connect.php");?>
 <?php include("getuser.php");?>
 <?php
-	$mon=getCash();
+	$mon=POSTCash();
 	include("../game.php");
-	echo $_GET['id'];
-	if($_GET['id']==1){
+	//echo $_POST['id'];
+	if($_POST['id']==1){
 	$string="{$mon}-";
 	$sql="SELECT * FROM gamedata WHERE userid={$usid}";
-	$result=mysql_query($sql,$con);
+	$result=mysql_query($sql);
 	$row=mysql_fetch_array($result);
 	$string .= "{$row['u1']}-{$row['u2']}-{$row['c1']}-{$row['c2']}-{$row['c3']}";
 	echo $string;
 	}
-	else if($_GET['id']==2){
+	else if($_POST['id']==2){
 		
 	$string="{$mon}-";
 	$sql="SELECT * FROM gamedata WHERE userid={$usid}";
-	$result=mysql_query($sql,$con);
+	$result=mysql_query($sql);
 	$row=mysql_fetch_array($result);
 	$string .= "{$row['c4']}-{$row['c5']}-{$row['d1']}-{$row['d2']}";
 	echo $string;
