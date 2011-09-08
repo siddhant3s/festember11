9a474aac5dfbfde59d81102d3e3b74a01b58d338
Amrit <amritsahoo@gmail.com>
2011-09-09 01:15:11 +0530

    added SQL injection prevention

--- apps/blackjack/scoreput.php
+++ apps/blackjack/scoreput.php
@@ -12,13 +12,13 @@ $sql="SELECT * FROM game_info WHERE playerid = '".$user['id']."' AND gameid = 2 
 $result = mysql_num_rows(mysql_query($sql));
 if(!$result)
 {
-$sql="INSERT INTO game_info (playerid,gameid,starttime,bidamount,returnpercent) VALUES('".$user[id]."',2,CURRENT_TIMESTAMP,'".$_POST["bid"]."','".$_POST["ret"]."' ) ";
+$sql="INSERT INTO game_info (playerid,gameid,starttime,bidamount,returnpercent) VALUES('".$user[id]."',2,CURRENT_TIMESTAMP,'".mysql_real_escape_string($_POST["bid"])."','".mysql_real_escape_string($_POST["ret"])."' ) ";
 
 $result = mysql_query($sql);
 }
 else
 {
-$sql="UPDATE game_info SET returnpercent ='".$_POST["ret"]."', endtime = now(),bidamount='".$_POST["bid"]."' WHERE playerid = '".$user[id]."' AND gameid = 2  ";
+$sql="UPDATE game_info SET returnpercent ='".mysql_real_escape_string($_POST["ret"])."', endtime = now(),bidamount='".mysql_real_escape_string($_POST["bid"])."' WHERE playerid = '".$user[id]."' AND gameid = 2  ";
 $result = mysql_query($sql);
 }
 
