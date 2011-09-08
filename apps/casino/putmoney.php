c1d5e80258c3c21533dceec01ebfa7581323ff69
SHADY <shady@SHADY.(none)>
2011-09-08 19:29:27 +0530

    test

--- apps/casino/putmoney.php
+++ apps/casino/putmoney.php
@@ -7,9 +7,8 @@
 	$row=mysql_fetch_array($result);
 	$sql="DELETE FROM gamedata WHERE userid={$usid}";
 	$result=mysql_query($sql,$con);
-	$sql="INSERT INTO gamedata VALUES({$row['userid']},{$row['u1']},{$row['u2']},{$row['d1']},{$row['d2']},{$row['c1']},{$row['c2']},{$row['c3']},{$row['c4']},{$row['c5']},{$_GET['money']})";
+	$sql="INSERT INTO gamedata VALUES({$row['userid']},{$row['u1']},{$row['u2']},{$row['d1']},{$row['d2']},{$row['c1']},{$row['c2']},{$row['c3']},{$row['c4']},{$row['c5']},{$_POST['money']})";
 	$result=mysql_query($sql,$con);
-	$res=mysql_query("INSERT INTO game_info ('playerid','gameid','starttime','endtime','timediff','bidamount','returnpercent') VALUES ({$user["id"]},{$game_array['poker']},{time()},{time()},'0000-00-00 00:00:00',{$_GET['money']},0)");
-	echo "INSERT INTO game_info ('playerid','gameid','starttime','endtime','timediff','bidamount','returnpercent') VALUES ({$user["id"]},{$game_array['poker']},{time()},{time()},'0000-00-00 00:00:00',{$_GET['money']},0)";
+	$res=mysql_query("INSERT INTO game_info ('playerid','gameid','starttime','endtime','timediff','bidamount','returnpercent') VALUES ({$user["id"]},{$game_array['poker']},{time()},{time()},'0000-00-00 00:00:00',{$_POST['money']},0)");
 	echo "";
 ?>
