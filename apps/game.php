2486983ab627317fb317e5b93e8f6119de908807
Shiva Nandan <seekshiva@gmail.com>
2011-09-08 18:44:12 +0530

    testing on the result obtained from mysql

--- apps/game.php
+++ apps/game.php
@@ -6,13 +6,14 @@ include("fb.php");
 function getCash() {
    $query = "SELECT `bidamount`,`winvar` FROM `game_info` WHERE `playerid`='" . $user["id"]  . "'"; //" AND `end_time` != '0'";
    $res = mysql_query($query);
-
+   echo $query;
    $cash = 1000;
    
    $sarr["0"] = -1;
    $sarr["1"] = 1;
 
    while ($row = mysql_fetch_array($res)) {
+       print_r($row);
        $cash += $row['bidamount'] * $row[$sarr['winvar']];
    }
    
