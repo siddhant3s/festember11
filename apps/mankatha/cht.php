0bb4216a242ac888296a2b21a54aa720fa19a909
Harish <harishsmart93@yahoo.co.in>
2011-09-09 01:30:48 +0530

    checking cht.php

--- apps/mankatha/cht.php
+++ apps/mankatha/cht.php
@@ -1,30 +1,30 @@
 <?php
-
 $rpath = "../";
   include("../game.php");
-	    $q="INSERT into game_info (`playerid`,`gameid`, `starttime`,`bidamount`) VALUES(" . $user["id"] . ",'4' , now(),{$_SESSION['cht']})";
-
+include("../../connect.php");
 if(!isset($_POST['txtchar']))
 {
 	header('Location:start.php');
 	exit;
 }
-include("../../connect.php");
-
-if(isset($_POST['txtchar']))
+else
   { $money=0;
     $money=getCash();
       	if($_POST['txtchar']>$money)
 	  { 
 	    header('Location:start.php?alertnobalance=1');
+	    exit;
 	  }
 	else
 	  { $_SESSION['cht']=$_POST['txtchar'];
 	    $date=date_create();
 
 	    $d=date_format($date,'Y-m-d H:i:s');
-	      header('Location:test1.php');
+	    $q="INSERT into game_info (starttime,bidamount) VALUES(now(),{$_SESSION['cht']}) where playerid='" . $user["id"] . "'";
+	      mysql_query($q);
+	    header('Location:test1.php');
+	    exit;
 	  }
-	exit;
+       
   }
 ?>	
