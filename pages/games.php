fa1a48e5a074d4ea146a5d4a8ceb37dbb788d4fc
Dinesh Prasanth <mail2dineshnow@gmail.com>
2011-09-09 17:48:11 +0530

    added games image

--- pages/games.php
+++ pages/games.php
@@ -1,4 +1,5 @@
 <div class="content-head">GAMES</div>
 <div class="content-body">
+<img style="width:400px;border:1px solid #333333;" src="images/gamepic.jpg">
 <p>Watch out for upcoming Festember Games..</p>
 </div>
