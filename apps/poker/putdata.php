942d714188797376fdb601da0617591766b4badc
SHADY <shady@SHADY.(none)>
2011-09-09 17:44:08 +0530

    bugfix

--- apps/poker/putdata.php
+++ apps/poker/putdata.php
@@ -17,8 +17,7 @@
 	$c5=$cards[8];
 	$sql="SELECT * FROM gamedata WHERE userid={$usid}";
 	$result=mysql_query($sql);
-	$row=mysql_fetch_array($result);
-	if(mysql_num_rows($row)==0){
+	if(mysql_num_rows($rseult)==0){
 		$sql="INSERT INTO gamedata VALUES({$usid},{$u1},{$u2},{$d1},{$d2},{$c1},{$c2},{$c3},{$c4},{$c5})";
 		$result=mysql_query($sql);
 	}
