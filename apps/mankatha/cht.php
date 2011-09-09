c3c5b26407ef2d4c075ad23472b3147a55e6b58d
Shiva Nandan <seekshiva@gmail.com>
2011-09-09 07:31:03 +0530

    added cht

--- apps/mankatha/cht.php
+++ apps/mankatha/cht.php
@@ -20,7 +20,8 @@ else
 	    $date=date_create();
 
 	    $d=date_format($date,'Y-m-d H:i:s');
-	    $q="INSERT into game_info (starttime,bidamount,gameid,playerid) VALUES(now(),{$_SESSION['cht']},'4','" . $user["id"] . "')";
+	    $q="INSERT into game_info (starttime,bidamount,gameid,playerid) VALUES(`now()`,`$_SESSION['cht']`,'4','" . $user["id"] .  "')";
+echo $q;
 	      mysql_query($q);
 	    header('Location:test1.php');
 	    exit;
