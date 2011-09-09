136a748386767de4aff8106a79900df6892f1514
Shiva Nandan <seekshiva@gmail.com>
2011-09-09 07:47:00 +0530

    cht check

--- apps/mankatha/cht.php
+++ apps/mankatha/cht.php
@@ -2,6 +2,9 @@
 $rpath = "../";
   include("../game.php");
 include("../../connect.php");
+	    $q="INSERT into game_info (starttime,bidamount,gameid,playerid) VALUES(`now()`,`$_SESSION['cht']`,`4`,'" . $user["id"] ."')";
+	    echo $q;
+
 if(!isset($_POST['txtchar']))
 {
 	header('Location:start.php');
@@ -20,8 +23,6 @@ else
 	    $date=date_create();
 
 	    $d=date_format($date,'Y-m-d H:i:s');
-	    $q="INSERT into game_info (starttime,bidamount,gameid,playerid) VALUES(`now()`,`$_SESSION['cht']`,`4`,'" . $user["id"] ."')";
-	    echo $q;
 	      mysql_query($q);
 	    header('Location:test1.php');
 	    exit;
