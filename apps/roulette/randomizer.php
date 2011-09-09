e23fa46db2605ab58cb03679e8b4ac74e0fae101
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-09-09 18:16:04 +0530

    Added more on duplicate and changed ajax call3

--- apps/roulette/randomizer.php
+++ apps/roulette/randomizer.php
@@ -9,7 +9,7 @@ $val=0;
    $val=round(($min+lcg_value()*(abs($max-$min))),3);
    $bal=round(fmod($val,0.005),3);
    $val=$val-($bal);
-   $sql="INSERT INTO roulette_verify VALUES (NULL,{$usid},{$val},'-1') ON DUPLICATE KEY UPDATE stoptime={$val} AND predict_num='-1'";
+   $sql="INSERT INTO roulette_verify VALUES (NULL,{$usid},{$val},'-1') ON DUPLICATE KEY UPDATE stoptime={$val}";
    $result=mysql_query($sql);
    echo $val;
 
