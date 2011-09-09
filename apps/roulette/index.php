45750538724f0423d2eb31f18772352e9e8ed139
judenaveenraj <judenaveenraj@yahoo.co.in>
2011-09-09 17:22:25 +0530

    Fixing getcash and getxp4

--- apps/roulette/index.php
+++ apps/roulette/index.php
@@ -27,6 +27,14 @@ include("../game.php");
 <script src="../gameapi.js" type="text/javascript"></script>
 <script language='javascript'>
 console.log("<?php echo getCash(); echo $user["name"]; echo $user["id"];  ?>");
+function getMyCash() {
+return <?php echo getCash(); ?>
+}
+
+function getMyXP() {
+return <?php echo getXP(); ?>
+}
+
 function boastcash() {
       obj = {
           name:"<?php echo $user["name"]; ?> has won the game of Roulette in Festember Casino!",
