7d5323dc9fbbe40f4c2926e636a9eb7c6ded1f98
Shiva Nandan <seekshiva@gmail.com>
2011-09-09 17:34:32 +0530

    minor bug fix in mankatha

--- apps/mankatha/cht.php
+++ apps/mankatha/cht.php
@@ -1,34 +1,30 @@
 <?php
 $rpath = "../";
-       include("../game.php");
-       include("../../connect.php");
-$q="INSERT INTO game_info( `starttime` , `bidamount` , `gameid` , `playerid`    VALUES ('now()', ".$_SESSION['cht'].", '4', '" . $user["id"] ."' )";
+include("../game.php");
+include("../../connect.php");
 
-	    	    echo $q;
-
-		    if(!isset($_POST['txtchar']))
-		    {
-		    	header('Location:start.php');
-				exit;
-				}
-				else
-				  { $money=0;
-				      $money=getCash();
-				      echo $money;
-				            	if($_POST['txtchar']>$money)
-							  { 
-							  	    header('Location:start.php?alertnobalance=1');
-								    	    exit;
-									    	  }
-										  	else
-												  { $_SESSION['cht']=$_POST['txtchar'];
-												  	   
-
-													  	  														    	      mysql_query($q);
-																													      
-															      	    header('Location:test1.php');
-																    	    exit;
-																	    	  }
-																		         
-																			   }
-																			   ?>	
+if(!isset($_POST['txtchar']))
+{
+    	header('Location:start.php');
+	exit;
+}
+else
+{
+	$money=0;
+	$money=getCash();
+	echo $money;
+	if($_POST['txtchar']>$money)
+	{ 
+	    header('Location:start.php?alertnobalance=1');
+	    exit;
+	}
+	else
+	{
+	    $_SESSION['cht']=$_POST['txtchar'];
+	    $q="INSERT INTO game_info( `starttime` , `bidamount` , `gameid` , `playerid`    VALUES (now(), ".$_SESSION['cht'].", '4', '" . $user["id"] ."' )";
+	    mysql_query($q);
+      	    header('Location:test1.php');
+    	    exit;
+	}
+}
+?>	
