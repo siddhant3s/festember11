ed42fad5124ec0677bab94d1e9958b057b68f30c
Shiva Nandan <seekshiva@gmail.com>
2011-09-09 18:15:23 +0530

    fixed a huge blunder in mankatha

--- apps/mankatha/cht.php
+++ apps/mankatha/cht.php
@@ -19,8 +19,7 @@ else
 	else
 	{
 	    $_SESSION['cht']=$_POST['txtchar'];
-	    $q="INSERT INTO game_info( `starttime` , `bidamount` , `gameid` , `playerid`    VALUES (now(), ".$_SESSION['cht'].", '4', '" . $user["id"] ."' )";
-	    error_log("###### sql query - " . $q);
+	    $q="INSERT INTO game_info( `starttime` , `bidamount` , `gameid` , `playerid`)  VALUES (now(), ".$_SESSION['cht'].", '4', '" . $user["id"] ."' )";
 	    mysql_query($q) or die("failed to execute query!");
       	    header('Location:test1.php');
     	    exit;
