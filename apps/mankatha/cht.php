ad5652a71dac95b2c04742eb6ab5676241ccb6f2
Shiva Nandan <seekshiva@gmail.com>
2011-09-09 07:56:04 +0530

    cht check5

--- apps/mankatha/cht.php
+++ apps/mankatha/cht.php
@@ -1,32 +1,32 @@
 <?php
 $rpath = "../";
-  include("../game.php");
-include("../../connect.php");
-	    $q="INSERT into game_info (starttime,bidamount,gameid,playerid) VALUES(now(),'$_SESSION[\'cht\']','4','" . $user["id"] ."')";
-	    echo $q;
+       include("../game.php");
+       include("../../connect.php");
+       	    $q="INSERT into game_info (starttime,bidamount,gameid,playerid) VALUES(now(),   '".$_SESSION['cht']."', '4','" . $user["id"] ."')";
+	    	    echo $q;
 
-if(!isset($_POST['txtchar']))
-{
-	header('Location:start.php');
-	exit;
-}
-else
-  { $money=0;
-    $money=getCash();
-      	if($_POST['txtchar']>$money)
-	  { 
-	    header('Location:start.php?alertnobalance=1');
-	    exit;
-	  }
-	else
-	  { $_SESSION['cht']=$_POST['txtchar'];
-	    $date=date_create();
+		    if(!isset($_POST['txtchar']))
+		    {
+		    	header('Location:start.php');
+				exit;
+				}
+				else
+				  { $money=0;
+				      $money=getCash();
+				            	if($_POST['txtchar']>$money)
+							  { 
+							  	    header('Location:start.php?alertnobalance=1');
+								    	    exit;
+									    	  }
+										  	else
+												  { $_SESSION['cht']=$_POST['txtchar'];
+												  	    $date=date_create();
 
-	    $d=date_format($date,'Y-m-d H:i:s');
-	      mysql_query($q);
-	    header('Location:test1.php');
-	    exit;
-	  }
-       
-  }
-?>	
+													    	    $d=date_format($date,'Y-m-d H:i:s');
+														    	      mysql_query($q);
+															      	    header('Location:test1.php');
+																    	    exit;
+																	    	  }
+																		         
+																			   }
+																			   ?>	
