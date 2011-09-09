21ff3d9486292e0b369cfd1a465e6ef99a98b93f
SHADY <shady@SHADY.(none)>
2011-09-09 18:48:10 +0530

    test

--- apps/poker/putdata.php
+++ apps/poker/putdata.php
@@ -15,8 +15,8 @@
 	$c3=$cards[6];
 	$c4=$cards[7];
 	$c5=$cards[8];
-	//$sql="DELETE FROM gamedata WHERE userid={$usid}";
-	//$result=mysql_query($sql);
+	$sql="DELETE FROM gamedata WHERE userid={$usid}";
+	$result=mysql_query($sql);
 	$sql="INSERT INTO gamedata VALUES({$usid},{$u1},{$u2},{$d1},{$d2},{$c1},{$c2},{$c3},{$c4},{$c5},0)";
 	$result=mysql_query($sql);
 	echo getCash();
