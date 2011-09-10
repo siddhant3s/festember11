b7d3b715f343e117740db403cd371fedb418f8df
Mythreya Raj <mythreyaraj@gmail.com>
2011-09-11 03:19:16 +0530

    iframe try

--- apps/mankatha/index.php
+++ apps/mankatha/index.php
@@ -2,26 +2,6 @@
 <head>
 <title>welcome to mankatha</title>
 <style type="text/css">
-body
-{
-	color:white;
-}
-#bimage
-{	position:absolute;
- left:250px;
-height:655px;
-z-index:-1;
-}
-#centerdiv{
-	position:absolute;
-	left:300px;;
-	top:430px;
-}
-#centerdiv2{
-	position:absolute;
-	left:870px;
-	top:430px;
-}
 #frame iframe{
 	margin:0px;
 	padding:0px;
@@ -30,21 +10,22 @@ z-index:-1;
 	margin:0px;
 	padding:0px;
 	height:100px;
+
+}
+#frame1 iframe{
+	margin:0px;
+	padding:0px;
+}
+#frame1{
+	margin:0px;
+	padding:0px;
+	height:100px;
 }
 </style>
-<script src="jquery.js"></script>
-<script type="text/javascript">
-</script>
-
 </head>
-
-
 <body>
 <div id="frame"><iframe src="../header.php" scrolling="no" frameborder="0" width="870" id="header"></iframe></div>
-<img src="mangatha cover.png" alt="" id="bimage"/>
-<a href="start.php" id="centerdiv"><img
-name="jsbutton" id="centered"  src="bs.png" border="0" alt=""></a>
-<a href="rules.html"  id="centerdiv2"><img name="jsbutton" id="centered"  src="br.png" border="0" alt="javascript button"></a>
+<div id="frame1"><iframe src="index2.php" scrolling="no" frameborder="0" width="870" id="header"></iframe></div>
 </body>
 
 </html>
---
+++ apps/mankatha/index2.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title>welcome to mankatha</title>
+<style type="text/css">
+body
+{
+	color:white;
+}
+#bimage
+{	position:absolute;
+ left:250px;
+height:655px;
+z-index:-1;
+}
+#centerdiv{
+	position:absolute;
+	left:300px;;
+	top:430px;
+}
+#centerdiv2{
+	position:absolute;
+	left:870px;
+	top:430px;
+}
+#frame iframe{
+	margin:0px;
+	padding:0px;
+}
+#frame{
+	margin:0px;
+	padding:0px;
+	height:100px;
+}
+</style>
+<script src="jquery.js"></script>
+<script type="text/javascript">
+</script>
+
+</head>
+
+
+<body>
+<div id="frame"><iframe src="../header.php" scrolling="no" frameborder="0" width="870" id="header"></iframe></div>
+<img src="mangatha cover.png" alt="" id="bimage"/>
+<a href="start.php" id="centerdiv"><img
+name="jsbutton" id="centered"  src="bs.png" border="0" alt=""></a>
+<a href="rules.html"  id="centerdiv2"><img name="jsbutton" id="centered"  src="br.png" border="0" alt="javascript button"></a>
+</body>
+
+</html>
