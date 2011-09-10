3b7be2b2baeba68ad5138d84f2a9b73db224c7bd
SHADY <shady@SHADY.(none)>
2011-09-10 20:43:48 +0530

    .

--- apps/header.php
+++ apps/header.php
@@ -20,15 +20,15 @@
 			#cash1{
 				color:white;
 				position:absolute;
-				left:40px;
-				top:40px;
+				left:110px;
+				top:35px;
 				
 			}
 			#star1{
 				color:white;
 				position:absolute;
-				right:110px;
-				top:35px;
+				right:40px;
+				top:40px;
 			}
 			
 		</style>
@@ -56,11 +56,11 @@
 			<iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fapps%2Fapplication.php%3Fid%3D263593520331172%26sk%3Dwall&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;border_color=%23444&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:62px;" allowTransparency="true" style="position:absolute; "></iframe>
 			<div id="xp">
 				<img src="images/star.png" class="logo"/>
-				<p id="star1"></p>
+				<b><p id="star1"></p></b>
 			</div>
 			<div id="cash">
 				<img src="images/cash.png" class="logo"/>
-				<p id="cash1"></p>
+				<b><p id="cash1"></p></b>
 			</div>
 		</div>		
 	</body>
