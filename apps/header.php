fecea799cf2d64b2f328fb35e5c0493c0e3515b1
SHADY <shady@SHADY.(none)>
2011-09-10 19:10:08 +0530

    header added

---
+++ apps/header.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>header</title>
+		<style type="text/css">
+			#wrapper{
+				width:100%;
+				height:100px;
+			}
+			#xp{
+				float:right;
+			}
+			#cash{
+				float:left;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="xp">
+			</div>
+			<div id="cash">
+			</div>
+		</div>		
+	</body>
+</html>
