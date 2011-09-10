ce362698f0b5bbd2467b43427d90597b552a6bd0
SHADY <shady@SHADY.(none)>
2011-09-10 20:26:57 +0530

    changing images

--- apps/header.php
+++ apps/header.php
@@ -19,10 +19,12 @@
 			}
 			#cash1{
 				float:right;
+				
 			}
 			#star1{
 				float:left;
 			}
+			
 		</style>
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js" ></script>
 		<script type="text/javascript">
@@ -37,6 +39,7 @@
 	</head>
 	<body>
 		<div id="wrapper">
+			<iframe src="http://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fapps%2Fapplication.php%3Fid%3D263593520331172%26sk%3Dwall&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;border_color=%23444&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:62px;" allowTransparency="true" style="position:absolute; "></iframe>
 			<div id="xp">
 				<img src="images/star.png" class="logo"/>
 				<p id="star1"></p>
