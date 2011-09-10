4b60d190fcff601f0f7f6ad69649e61790e6b8c7
SHADY <shady@SHADY.(none)>
2011-09-11 00:13:56 +0530

    .

--- apps/header.php
+++ apps/header.php
@@ -98,7 +98,7 @@
 }(document));</script>
 <div class="fb-like" data-href="http://apps.facebook.com/festigame/" data-send="false" data-layout="button_count" data-width="50" data-show-faces="true"></div>
 			<div id="fbinfo">
-				<div id="ph"><fb:profile-pic uid="<?php echo $user["id"]; ?>" linked="true" /></div>
+				<div><div id="ph"><fb:profile-pic uid="<?php echo $user["id"]; ?>" linked="true" /></div></div>
 				<p><?php echo $user["name"]; ?></p>	
 			</div>
 			
