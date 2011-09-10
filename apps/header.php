3751850e73ac78bc717e657214a5480c64034c86
SHADY <shady@SHADY.(none)>
2011-09-10 19:17:56 +0530

    test

--- apps/header.php
+++ apps/header.php
@@ -5,6 +5,7 @@
 			#wrapper{
 				width:100%;
 				height:100px;
+				
 			}
 			#xp{
 				float:right;
@@ -12,13 +13,19 @@
 			#cash{
 				float:left;
 			}
+			.logo{
+				 width:75px;
+				 height:75px;
+			}
 		</style>
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="xp">
+				<img src="images/star.png" class="logo"/>
 			</div>
 			<div id="cash">
+				<img src="images/cash.png" class="logo"/>
 			</div>
 		</div>		
 	</body>
