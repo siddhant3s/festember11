37718cb6f47850c1e2275179e2d98eede59c61eb
Mythreya Raj <mythreyaraj@gmail.com>
2011-09-11 04:49:57 +0530

    try

--- apps/mankatha/dell.php
+++ apps/mankatha/dell.php
@@ -17,10 +17,10 @@ unset($_SESSION['cht']);
 <title>you lost</title>
 <body>
 <div id="wrapper" style="background-image:url('mangatha cover.png'); width:800px; height:600px;">
-<div style="position:absolute; top:50%; left:45%; color:white;"?>
+<div style="color:white;"?>
 You lost this round.
-<a href="start.php" style="position:absolute;left:5%;top:70%;"><img src="bpa.png" border="0" alt=""/></a>
-<a href="index.php" style="position:absolute;left:5%;top:650%;"><img src="bq.png" border="0" alt=""/></a>
+<a href="start.php" style="position:absolute;left:50px;top:400px;"><img src="bpa.png" border="0" alt=""/></a>
+<a href="index.php" style="position:absolute;rigt:50px;top:400px;"><img src="bq.png" border="0" alt=""/></a>
 </div>
 </div>
 </body>
