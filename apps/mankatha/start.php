594cce377bc1c14757253f9da4ae4c250849f6c5
Mythreya Raj <mythreyaraj@gmail.com>
2011-09-11 02:50:11 +0530

    bugfix

--- apps/mankatha/start.php
+++ apps/mankatha/start.php
@@ -76,7 +76,7 @@ function checkNumbers()
 <img alt="full screen background image" src="mangatgha.png" id="bimage">
 <div id="pos">
 <p>Enter bid</p>
-<form action="cht.php" name="myform" onSubmit="return checkNumbers()" method="post">
+<form action="test1.php" name="myform" onSubmit="return checkNumbers()" method="post">
 
 <input id="txtchar"  type="text" name="txtchar" 
 onkeypress="return onlyNumbers(event);">
