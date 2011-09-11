c9b09b3150a6d4b2a853a03572c292f24a77d0dd
Mythreya Raj <mythreyaraj@gmail.com>
2011-09-11 15:04:57 +0530

    center aligin

--- apps/mankatha/index.php
+++ apps/mankatha/index.php
@@ -21,11 +21,17 @@
 	padding:0px;
 	height:655px;
 }
+#wrapper{
+	margin:0 auto;
+	width:800px;
+}
 </style>
 </head>
 <body>
+<div id="wrapper">
 <div id="frame"><iframe src="../header.php" scrolling="no" frameborder="0" width="800"  id="header"></iframe></div>
 <div id="frame1"><iframe src="index2.php" scrolling="no" frameborder="0" width="800" height="600" id="header"></iframe></div>
+</div>
 </body>
 
 </html>
