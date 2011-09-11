becad882f1359a0183ecc4f53dde33eb7a803178
Shiva Nandan <seekshiva@gmail.com>
2011-09-11 07:13:29 +0530

    added some extra error logging for roulette

--- apps/roulette/setscorebegin.php
+++ apps/roulette/setscorebegin.php
@@ -1,6 +1,7 @@
 <?php
 	error_log("#####_______________Playerid inside setscorebegin.php:".$usid);
 	$sql="INSERT INTO game_info (playerid,gameid,starttime,bidamount) VALUES (" . $usid . ",{$game_array['roulette']},now(),{$_POST['money']})";
+	error_log("#####_______________Playerid inside setscorebegin.php:".$usid);	
 	$res=mysql_query($sql);
 	$insertid=mysql_insert_id();
 	
