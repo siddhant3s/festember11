e8d6f06cdb49dfd2285b8e3fc9d210982f7defea
Mythreya Raj <mythreyaraj@gmail.com>
2011-09-13 01:13:02 +0530

    added link

--- pages/games.php
+++ pages/games.php
@@ -1,5 +1,5 @@
 <div class="content-head">GAMES</div>
 <div class="content-body">
 <img style="width:400px;border:1px solid #333333;" src="images/gamepic.jpg">
-<p>Watch out for upcoming Festember Games..</p>
+<a href="http://festember.in/11/apps" style="text-decoration:none; color:#EEOO2D; font-size:20px; font-weight:bold;">CLICK HERE</a>
 </div>
