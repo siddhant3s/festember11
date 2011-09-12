570cf5b0ef401d53f6145183af97221bcc8bc460
Mythreya Raj <mythreyaraj@gmail.com>
2011-09-13 01:15:39 +0530

    added link

--- pages/games.php
+++ pages/games.php
@@ -1,5 +1,5 @@
 <div class="content-head">GAMES</div>
 <div class="content-body">
 <img style="width:400px;border:1px solid #333333;" src="images/gamepic.jpg">
-<a href="http://festember.in/11/apps" style="text-decoration:none; color:#EEOO2D; font-size:20px; font-weight:bold;">CLICK HERE</a>
+<a href="http://festember.in/11/apps" style="text-decoration:none; color:#EE002D; font-size:20px; font-weight:bold; font-family: Oswald,MyraidPro;">CLICK HERE</a>
 </div>
