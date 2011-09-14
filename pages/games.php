8136abb4a9d2d5727cf923e32a1f7cd0840944d7
Boopathi Rajaa <boopathi@live.com>
2011-09-14 15:04:57 +0530

    bringing the services up on festember

--- pages/games.php
+++ pages/games.php
@@ -1,5 +1,5 @@
 <div class="content-head">GAMES</div>
 <div class="content-body">
 <img style="width:400px;border:1px solid #333333;" src="images/gamepic.jpg"></br>
-coming up soon!
+<a href="apps">Click Here</a>
 </div>
