88f4acbfd3d604b98830bd4c72ffe9c51f1f82c1
Boopathi Rajaa <boopathi@live.com>
2011-09-14 22:57:51 +0530

    games.php

--- pages/games.php
+++ pages/games.php
@@ -4,7 +4,7 @@
 <img width="200" style="border:1px solid #333333;" src="images/gamepic.jpg"></br>
  </p>
 <p>
-Win amazing prizes, food coupons, and t-shirts @Festember by winning Games and scoring experience points.</p>
+Win amazing prizes, food coupons, and t-shirts @Festember by winning virtual cash and scoring experience points.</p>
 <p>Login using your facebook account and 
 <a href="apps">Click Here</a>
 to start playing Games.
