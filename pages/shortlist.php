a64160a9429c210f9650cd9da26c6a8760c9fe8e
Boopathi Rajaa <boopathi@live.com>
2011-09-14 11:40:29 +0530

    shortlist.php

---
+++ pages/shortlist.php
@@ -0,0 +1,19 @@
+<div class="content-head">
+Shortlisted teams
+</div>
+<div class="content-body">
+<div class="content-head">Theatrix</div>
+<ul>
+<li>Amritha college of engineering - Team 1(Karthik)</li>
+
+<li>Amritha college of engineering - Team 2(Anurag)</li>
+
+<li>Amritha college of engineering - Team 3(Ashwin)</li>
+
+<li>Vellore Institute of Technology</li>
+
+<li>Sastra, Thanjavore</li>
+
+<li>Government college of engineering, Salem</li>
+</ul>
+</div>
