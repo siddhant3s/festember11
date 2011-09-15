16f30c4841a6e0feb264802238fe24785015f8ce
rubulh <rubulhaloi2007@gmail.com>
2011-09-16 00:26:14 +0530

    debugging moderator.php

--- apps/ants/new_user.php
+++ apps/ants/new_user.php
@@ -2,16 +2,13 @@
 global $rpath;
 $rpath="../";
 require_once("../game.php");
-var_dump($user);
 
-/*
 function new_user()
 {
 global $user;
 global $table_allusers;
 $the_id=$user['id'];
 var_dump($user);
-exit(1);
 error_log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@".var_dump($user));
 $result_insert_new_user=mysql_query("insert into '$table_allusers' (user_id,logged) values('$the_id',1)");
 error_log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@".mysql_error());
@@ -26,5 +23,5 @@ $answer_insert_new_user=mysql_affected_rows();
 return $user;
 }
 var_dump(new_user())
-exit();*/
+exit();
 ?>
