58ae226161c8a6f6a2c4704a8a00d6a0a3f5cfb5
rubulh <rubulhaloi2007@gmail.com>
2011-09-15 23:59:36 +0530

    debugging moderator.php

--- apps/ants/new_user.php
+++ apps/ants/new_user.php
@@ -6,7 +6,7 @@ $the_id=$user['id'];
 $result_insert_new_user=mysql_query("insert into '$table_allusers' (user_id,logged) values('$the_id',1)");
 error_log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@".mysql_error());
 	//might need to include a key for theis in the root database ;later
-$answer_insert_new_user=mysql_affected_rows($the_conn);
+$answer_insert_new_user=mysql_affected_rows();
 
 	if(!$answer_insert_new_user)
 		{
