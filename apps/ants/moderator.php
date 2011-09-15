81dbff6b9a4f05695221c79a61b67389be6afc31
rubulh <rubulhaloi2007@gmail.com>
2011-09-15 19:48:17 +0530

    debugging moderator.php

--- apps/ants/moderator.php
+++ apps/ants/moderator.php
@@ -18,7 +18,7 @@ if((isset($_POST['submit'])) &&( isset($_POST['bet'])) )
 	$done_match=0;
 $current_users_balance=getCash();
 $bet_entered=mysql_real_escape_string($_POST['bet']);
-
+var_dump($current_users_balance,$bet_entered,$the_fb_id);
    if(!is_numeric($bet_entered))
     {
     $the_error_code=1;
