0a268107fed7162ed20cc989d27687d97f4d6766
Amrit <amritsahoo@gmail.com>
2011-09-15 15:26:52 +0530

    changed score system

--- apps/blackjack/scoreput.php
+++ apps/blackjack/scoreput.php
@@ -8,7 +8,7 @@ include("../game.php");
 
 
 
-$sql="INSERT INTO game_info (playerid,gameid,starttime,bidamount,returnpercent) VALUES('".$user[id]."',2,CURRENT_TIMESTAMP,'".mysql_real_escape_string($_POST["bid"])."','".mysql_real_escape_string($_POST["ret"])."' ) ";
+$sql="INSERT INTO game_info (playerid,gameid,starttime,bidamount,returnpercent) VALUES('".$user["id"]."',2,CURRENT_TIMESTAMP,'".mysql_real_escape_string($_POST["bid"])."','".mysql_real_escape_string($_POST["ret"])."' ) ";
 
 $result = mysql_query($sql);
 
