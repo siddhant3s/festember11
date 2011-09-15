4fa1af7d5e12b2c8692a867422222624ea3887f6
rubulh <rubulhaloi2007@gmail.com>
2011-09-16 00:45:08 +0530

    debugging moderator.php

--- apps/ants/new_user.php
+++ apps/ants/new_user.php
@@ -5,9 +5,9 @@ function new_user()
 global $user;
 global $table_allusers;
 $the_id=$user['id'];
-error_log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@".var_dump($user));
+
 $result_insert_new_user=mysql_query("insert into $table_allusers (user_id,logged) values('$the_id',1)");
-error_log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@".mysql_error());
+
 	//might need to include a key for theis in the root database ;later
 $answer_insert_new_user=mysql_affected_rows();
 
