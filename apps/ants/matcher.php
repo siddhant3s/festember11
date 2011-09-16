5ed22eb9c90c35d40d53989f7542f8887a92f801
rubulh <rubulhaloi2007@gmail.com>
2011-09-16 16:37:46 +0530

    dynamic matching script

--- apps/ants/matcher.php
+++ apps/ants/matcher.php
@@ -20,7 +20,9 @@ $the_user_rating=get_the_rating();
 $thefinalsuccesstoken=0;
 //##############################################################
 $result_set_currently_matched=mysql_query("update $table_allusers set involved=2 where fb_id='$the_fb_id'");
+error_log("1#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 $answer_set_currently_matched=mysql_affected_rows();
+error_log("2#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 if(!$answer_set_currently_matched)
 		{
 			whisk(4);
@@ -39,7 +41,9 @@ while(false!=($his_id=mysql_fetch_array($answer_get_all_uninvloved_but_logged)))
 		else	{$this_case_difference=$his_rating-$the_user_rating;}
 		if($this_case_difference<$min_difference){	
 								$result_release_earlier_match=mysql_query("update $table_allusers set involved=2 where fb_id='$the_opponent'");
+								error_log("3#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 								$answer_release_earlier_match=mysql_affected_rows();
+								error_log("4#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 								if(!$answer_release_earlier_matched)
 									{
 										whisk(6);
@@ -50,7 +54,9 @@ while(false!=($his_id=mysql_fetch_array($answer_get_all_uninvloved_but_logged)))
 								$min_difference=$this_case_difference;
 								$the_opponent=$his_id['fb_id'];
 								$result_plot_this_match=mysql_query("update $table_allusers set involved=3 where fb_id='$the_opponent'");
+								error_log("5#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 								$answer_plot_this_match=mysql_affected_rows();
+								error_log("6#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 								if(!$answer_set_currently_matched)
 									{
 										whisk(5);
@@ -62,11 +68,15 @@ if($the_opponent)
 		{
 		
 		$result_confirm_opponent=mysql_query("select involved from $table_allusers where fb_id='$the_opponent'");
+		error_log("7#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 		$answer_confirm_opponent=mysql_fetch_array($result_confirm_opponent);
+		error_log("8#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 		if($answer_confirm_opponent==3)
 			{
 				$result_set_opponent1_match=mysql_query("update $table_allusers set opponent='$the_fb_id' where fb_id='$the_opponent'");
+				error_log("9#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 				$answer_set_opponent1_match=mysql_affected_rows();
+				error_log("10#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 				if(!$answer_set_opponent1_match)
 					{
 						whisk(7);
@@ -77,7 +87,9 @@ if($the_opponent)
 
 
 				$result_set_opponent2_match=mysql_query("update $table_allusers set opponent='$the_opponent' where fb_id='$the_fb_id'");
+				error_log("11#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 				$answer_set_opponent2_match=mysql_affected_rows();
+				error_log("12#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 				if(!$answer_set_opponent1_match)
 					{
 						whisk(8);
@@ -92,7 +104,9 @@ if($the_opponent)
 	else
 		{
 				$result_set_opponent2_match=mysql_query("update $table_allusers set opponent='AI' where fb_id='$the_fb_id'");
+				error_log("13#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 				$answer_set_opponent2_match=mysql_affected_rows();
+				error_log("14#!#!#!#!#!##!##!#!##!#!##!##!#".mysql_error());
 				if(!$answer_set_opponent1_match)
 					{
 						whisk(9);
