96b0e5951478f30596d85bc558784c6aab0a5dbd
rubulh <rubulhaloi2007@gmail.com>
2011-09-17 22:51:20 +0530

    integrating start_game.php

--- apps/ants/placeyourbet.php
+++ apps/ants/placeyourbet.php
@@ -1,7 +1,8 @@
 
 
-
+<div id="betform">
 <form method="POST" action="moderator.php">
-<input type="text" name="bet" style="width:200px;height:50px;border-radius:90px;font-size:30px;font-weight:700;background-color:red;color:blue;">
-<input type="submit" name="submit" id="proceed" style="width:70px;height:50px;border-radius:200px;font-size:30px;font-weight:700;background-color:red;color:blue;" value="go">
+<input type="text" name="bet" style="width:200px;height:50px;border-radius:90px;font-size:30px;font-weight:700;background-color:maroon;color:blue; " id="betinput">
+<button name="submit" id="proceed" style="width:70px;height:50px;border-radius:200px;font-size:30px;font-weight:700;background-color:maroon;color:black;">go</button>
 </form>
+</div>
