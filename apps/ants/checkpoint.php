bdf723dc9fd7e0991fc64b5f614bd72822d679be
rubulh <rubulhaloi2007@gmail.com>
2011-09-17 23:34:06 +0530

    checkpoint.php added

---
+++ apps/ants/checkpoint.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+<style type="text/css">
+body{background-image:url(back.jpg);opacity:1.0;}
+#complete{width:80%;height:99%;margin:auto;border-color:white;border-style:solid;}
+#picture{width:99%;height:300px;margin:auto;border-color:white;border-style:solid;}
+#details{width:99%%;height:100px;background-color:black;opacity:0.8;z-index:9;border-radius:35px;border-style:ridge;}
+</style>
+<script type="text/javascript" src="jquery-1.3.2.min.js">
+</script>
+</head>
+<body>
+<div id="complete">
+<div id="picture">
+</div>
+<div id="details">
+</div>
+</div>
+</body>
+</html>
