2f0ccebdaa7db17ede788d3144099e925aa9fbd1
rubulh <rubulhaloi2007@gmail.com>
2011-09-18 11:56:12 +0530

    added support for the turn function with optional argument as 00

--- apps/ants/turn.php
+++ apps/ants/turn.php
@@ -7,63 +7,84 @@
 	//user id
 //##############################################################
 	// if 1 set 
-		//randomise between 1 and 2 if
+		
 //##############################################################
-	//set the invloved keys to 1 in either of the user ids 
-	//shuffle the card stack
-	//set  active game counter=1
-	//activate
- //##############################################################
-
- //##############################################################
-	
-//##############################################################
-
 
 function turn($the_game_hash,$first=0)
 {
-global $conn_pag2;
+//##############################################################
+global $user;
 global $table_allusers;
 global $table_allgames;
 
+//##############################################################
+$value_to_be_returned="0";
+$the_first_turn_user=$user['id'];
 $this_turn=0;
+
+//##############################################################
+
+
 	if($first)
 	{
 //##############################################################
-			//SET THE R
-		$query_get_the_card_stack="select card_stack from '$table_allgames' where game_id='$the_game_hash'";
-		$result_get_the_card_stack=mysqli_query($conn_pag2,$query_get_the_card_stack);
-		$answer_get_the_card_stack=mysqli_fetch_array($result_get_the_card_stack);
+
+		$query_get_the_card_stack="select card_stack from $table_allgames where game_id='$the_game_hash'";
+		$result_get_the_card_stack=mysql_query($query_get_the_card_stack);
+		$answer_get_the_card_stack=mysql_fetch_array($result_get_the_card_stack);
 		$the_current_card_stack=$answer_get_the_card_stack['card_stack'];
 		$the_new_card_stack=preg_replace("/^0/",'R',$the_current_card_stack,1,$thisreplacementvariable);
 		if(!($thisreplacementvariable==1))
 			{
-			//take an action
+				whisk(56);
+				exit(1);
 			}
 		
 		//	$the_current_card_stack='R'.$the_current_card_stack;
-		$update_the_card_stack="update '$table_allgames' set card_stack='$the_current_card_stack' where game_id='$the_game_hash'";
-		$result_the_card_stack=mysqli_query($conn_pag2,$update_the_card_stack);
-			if(!mysqli_affected_rows($conn_pag2))
+		$update_the_card_stack="update $table_allgames set card_stack='$the_current_card_stack' where game_id='$the_game_hash'";
+		$result_the_card_stack=mysql_query($update_the_card_stack);
+			if(!mysql_affected_rows())
 				{
+					whisk(93);
+					exit(1);
 					//take an action
 					
 				}
 //##############################################################
+	//get the opponent
+$opponent_fetch=mysql_query("select opponent from $table_allusers where user_id='$the_first_turn_user'");
+$answer_opponent_fetch=mysql_fetch_array($opponent_fetch);
+			if(!$answer_opponent_fetch)
+				{
+					whisk(96);
+					exit(1);
+				}
+			else if($answer_opponent_fetch)
+				{
+					$opponent=$answer_opponent_fetch['opponent'];
+
+				}
+//##############################################################
 		//the person who reaches the page first gets the first turn
-		$the_first_turn_user=get_the_id();
-		return $the_first_turn_user;
-		exit(1);
+			if($opponent=='AI')
+				{
+				error_log("XXXXXXXXXXXXXX(((-1-)))XXXXXXXXXXXXXX".$the_game_hash."%%%%%%%%%%%%%%%%%".$the_first_turn_user);
+					return $the_first_turn_user;
+					exit(1);
+				}
+	//guess its redundant
+	error_log("XXXXXXXXXXXXXX(((-2-)))XXXXXXXXXXXXXX".$the_game_hash."%%%%%%%%%%%%%%%%%".$the_first_turn_user);
+	return $the_first_turn_user;
+	exit(1);
 		
 		
 	}
-
-
 	else
 	{
-		$the_last_turn=get_the_id($conn_pag2,);
-		
+		error_log("XXXXXXXXXXXXXX(((-3-)))XXXXXXXXXXXXXX".$the_game_hash."%%%%%%%%%%%%%%%%%".$the_last_turn);
+		$the_last_turn=$user['id'];
+		return $the_last_turn;
+		exit(1);		
 	}
 }
-
 ?>
